dc4d86dc4dbb282084be081c404e3457395b2a35
AlberLC <alberlc@outlook.com>
2022-01-15 02:28:40 +0100

    Add callable return_ to return_if_first_empty and reorder decorators

--- flanautils/decorators.py
+++ flanautils/decorators.py
@@ -6,6 +6,35 @@ from typing import Any, Callable, Iterable, Type
 from flanautils import iterables
 
 
+# --------------------------------------------------------- #
+# -------------------- META DECORATORS -------------------- #
+# --------------------------------------------------------- #
+def shift_args_if_called(func_: Callable = None, *, exclude_self_types: str | Type | Iterable[str | Type] = (), globals_: dict = None) -> Callable:
+    """Decorator for decorators that shifts the arguments depending on whether the decorator is called or not."""
+
+    if func_ is not None and not callable(func_):
+        func_, exclude_self_types, globals_ = iterables.shift_function_args(func_, exclude_self_types, globals_, func=shift_args_if_called)
+
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            self, args = iterables.separate_self_from_args(args, exclude_self_types, globals_)
+            if args and args[0] is not None and not callable(args[0]):
+                args = iterables.shift_function_args(*args, func=func)
+
+            if self:
+                return func(self, *args, **kwargs)
+            else:
+                return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator(func_) if func_ else decorator
+
+
+# ---------------------------------------------------- #
+# -------------------- DECORATORS -------------------- #
+# ---------------------------------------------------- #
 def do_after(after_function: Callable) -> Callable:
     """Decorator to execute a function or coroutine function after the decorated function."""
 
@@ -50,53 +79,6 @@ def do_before(before_function: Callable) -> Callable:
     return decorator
 
 
-def shift_args_if_called(func_: Callable = None, *, exclude_self_types: str | Type | Iterable[str | Type] = (), globals_: dict = None) -> Callable:
-    """Decorator for decorators that shifts the arguments depending on whether the decorator is called or not."""
-
-    if func_ is not None and not callable(func_):
-        func_, exclude_self_types, globals_ = iterables.shift_function_args(func_, exclude_self_types, globals_, func=shift_args_if_called)
-
-    def decorator(func):
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            self, args = iterables.separate_self_from_args(args, exclude_self_types, globals_)
-            if args and args[0] is not None and not callable(args[0]):
-                args = iterables.shift_function_args(*args, func=func)
-
-            if self:
-                return func(self, *args, **kwargs)
-            else:
-                return func(*args, **kwargs)
-
-        return wrapper
-
-    return decorator(func_) if func_ else decorator
-
-
-@shift_args_if_called
-def time_it(func_: Callable = None, /, n_executions=1) -> Callable:
-    """Decorator that prints the seconds it takes for the function to run."""
-
-    def decorator(func):
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            new_template = f"""def inner(_it, _timer{{init}}):
-                                   {{setup}}
-                                   _t0 = _timer()
-                                   for _i in _it:
-                                       result = {{stmt}}
-                                   _t1 = _timer()
-                                   print({repr(func.__name__)} + ': ' + str(_t1 - _t0) + ' seconds')
-                                   return result"""
-
-            timeit.template = new_template
-            return timeit.timeit(lambda: func(*args, **kwargs), number=n_executions)
-
-        return wrapper
-
-    return decorator(func_) if func_ else decorator
-
-
 @shift_args_if_called
 def repeat(func_: Callable = None, /, times=2) -> Callable:
     """Decorator that makes the decorated function be executed the specified number of times (by default times=2)."""
@@ -124,6 +106,8 @@ def return_if_first_empty(func_: Callable = None, /, return_: Any = None, exclud
     Ignore the first element if it is an instance of exclude_self_types.
     """
 
+    return_ = return_() if callable(return_) else return_
+
     def decorator(func: Callable) -> Callable:
         @functools.wraps(func)
         def wrapper(*args, **kwargs) -> Any:
@@ -140,6 +124,30 @@ def return_if_first_empty(func_: Callable = None, /, return_: Any = None, exclud
             if self:
                 args = (self, *args)
             return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator(func_) if func_ else decorator
+
+
+@shift_args_if_called
+def time_it(func_: Callable = None, /, n_executions=1) -> Callable:
+    """Decorator that prints the seconds it takes for the function to run."""
+
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            new_template = f"""def inner(_it, _timer{{init}}):
+                                   {{setup}}
+                                   _t0 = _timer()
+                                   for _i in _it:
+                                       result = {{stmt}}
+                                   _t1 = _timer()
+                                   print({repr(func.__name__)} + ': ' + str(_t1 - _t0) + ' seconds')
+                                   return result"""
+
+            timeit.template = new_template
+            return timeit.timeit(lambda: func(*args, **kwargs), number=n_executions)
 
         return wrapper
 
