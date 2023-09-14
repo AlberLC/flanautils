bebc9eb8d8fddc532c9d9c34c635d8834e098190
AlberLC <alberlc@outlook.com>
2023-09-14 19:19:31 +0200

    Add oss.next_path

--- flanautils/oss.py
+++ flanautils/oss.py
@@ -6,6 +6,7 @@ import subprocess
 import sys
 from collections.abc import Iterator
 from contextlib import contextmanager
+from typing import overload
 
 from flanautils import strings
 
@@ -23,6 +24,56 @@ def find_paths_by_stem(
 
     generator_ = (path for path in pathlib.Path(directory).iterdir() if str(path).split('.', maxsplit=1)[0] == stem)
     return generator_ if lazy else list(generator_)
+
+
+@overload
+def next_path(path_template: str, start=1, exhaustive=True) -> str:
+    pass
+
+
+@overload
+def next_path(path_template: pathlib.Path, start=1, exhaustive=True) -> pathlib.Path:
+    pass
+
+
+def next_path(path_template: str | pathlib.Path, start=1, exhaustive=True) -> str | pathlib.Path:
+    """
+    Finds the next free path in a sequentially named files.
+
+    path_template has to follow the syntax of str.format().
+
+    If exhaustive=True (the default) the function will take into account the gaps and ensure the lowest available index.
+    If not, it will use a more efficient algorithm to find a free index, but it will not necessarily be the lowest
+    possible.
+
+    >>> pathlib.Path('file_1.txt').touch()
+    >>> pathlib.Path('file_2.txt').touch()
+    >>> pathlib.Path('file_4.txt').touch()
+
+    >>> next_path('file_{}.txt')
+    'file_3.txt'
+    >>> next_path('file_{}.txt', exhaustive=False)
+    'file_5.txt'
+
+    >>> pathlib.Path('file_1.txt').unlink()
+    >>> pathlib.Path('file_2.txt').unlink()
+    >>> pathlib.Path('file_4.txt').unlink()
+    """
+
+    b = start
+
+    if exhaustive:
+        while pathlib.Path(path_template.format(b)).exists():
+            b += 1
+    else:
+        while pathlib.Path(path_template.format(b)).exists():
+            b *= 2
+        a, b = (b // 2, b)
+        while a + 1 < b:
+            c = (a + b) // 2
+            a, b = (c, b) if pathlib.Path(path_template.format(c)).exists() else (a, c)
+
+    return path_template.format(b)
 
 
 def resolve_path(path: str) -> pathlib.Path:
