d22b9a556dd72804ecee1bd95fc32b8a80e3355e
AlberLC <alberlc@outlook.com>
2024-05-09 20:17:09 +0200

    Update type hints

--- flanautils/asyncs.py
+++ flanautils/asyncs.py
@@ -5,14 +5,14 @@ import multiprocessing
 import queue
 from asyncio import Task
 from collections.abc import Callable, Iterable
-from typing import Any, Type
+from typing import Any, Never, Type
 
 
 async def _do(
     func: Callable,
-    *args,
+    *args: Any,
     exceptions_to_capture: Type[Exception] | Iterable[Type[Exception]] = (),
-    **kwargs
+    **kwargs: Any
 ) -> Any:
     """Execute the function whether it is asynchronous or not."""
 
@@ -22,12 +22,7 @@ async def _do(
         return e
 
 
-def _process_function(
-    func: Callable,
-    *args,
-    queue__: multiprocessing.Queue,
-    **kwargs
-):
+def _process_function(func: Callable, *args: Any, queue__: multiprocessing.Queue, **kwargs: Any) -> None:
     """Utility function for multiprocessing purposes."""
 
     if inspect.isawaitable(result := func(*args, **kwargs)):
@@ -39,11 +34,11 @@ def _process_function(
 def do_every(
     seconds: int | float | datetime.timedelta,
     func: Callable,
-    *args,
-    times: int = None,
+    *args: Any,
+    times: int | None = None,
     do_first_now=True,
     exceptions_to_capture: Type[Exception] | Iterable[Type[Exception]] = (),
-    **kwargs
+    **kwargs: Any
 ) -> Task:
     """
     Corutine function that runs the function every time provided in seconds or datetime.timedelta.
@@ -55,7 +50,7 @@ def do_every(
     Exceptions specified in exceptions_to_capture are returned if raised when the task is completed.
     """
 
-    async def do_every_(times_: int):
+    async def do_every_(times_: int) -> list | Never:
         if times_ is None:
             if do_first_now:
                 await _do(func, *args, exceptions_to_capture=exceptions_to_capture, **kwargs)
@@ -83,9 +78,9 @@ def do_every(
 def do_later(
     seconds: int | float | datetime.timedelta,
     func: Callable,
-    *args,
+    *args: Any,
     exceptions_to_capture: Type[Exception] | Iterable[Type[Exception]] = (),
-    **kwargs
+    **kwargs: Any
 ) -> Task:
     """
     Corutine function that executes the function after a time provided in seconds or datetime.timedelta.
@@ -93,7 +88,7 @@ def do_later(
     Exceptions specified in exceptions_to_capture are returned if raised.
     """
 
-    async def do_later_():
+    async def do_later_() -> Any:
         await asyncio.sleep(seconds)
         return await _do(func, *args, exceptions_to_capture=exceptions_to_capture, **kwargs)
 
@@ -105,7 +100,7 @@ def do_later(
     return asyncio.create_task(do_later_())
 
 
-async def poll_process(process: multiprocessing.Process, sleep_seconds=1):
+async def poll_process(process: multiprocessing.Process, sleep_seconds=1) -> None:
     """
     Starts the process and wait until the process is done.
 
@@ -117,7 +112,7 @@ async def poll_process(process: multiprocessing.Process, sleep_seconds=1):
         await asyncio.sleep(sleep_seconds)
 
 
-async def run_process(func: Callable, *args, timeout: int | float = None, **kwargs) -> Any:
+async def run_process(func: Callable, *args: Any, timeout: int | float | None = None, **kwargs: Any) -> Any:
     """
     Executes the function with the provided arguments in another process in parallel, waits asynchronously for its
     completion, and returns the result.
@@ -127,11 +122,7 @@ async def run_process(func: Callable, *args, timeout: int | float = None, **kwar
 
     queue__ = multiprocessing.Queue()
     await wait_for_process(
-        multiprocessing.Process(
-            target=_process_function,
-            args=(func, *args),
-            kwargs={'queue__': queue__, **kwargs}
-        ),
+        multiprocessing.Process(target=_process_function, args=(func, *args), kwargs={'queue__': queue__, **kwargs}),
         timeout
     )
     try:
@@ -140,7 +131,7 @@ async def run_process(func: Callable, *args, timeout: int | float = None, **kwar
         pass
 
 
-async def wait_for_process(process: multiprocessing.Process, timeout: int | float = None):
+async def wait_for_process(process: multiprocessing.Process, timeout: int | float | None = None) -> None:
     """
     Wrapper function that starts the process and wait until the process is done.
 
