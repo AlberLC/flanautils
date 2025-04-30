fa7e3df5621235c0528b46b9389218706022d361
AlberLC <alberlc@outlook.com>
2025-04-30 05:54:27 +0200

    Update .venv

--- tests/unit/test_docs.py
+++ tests/unit/test_docs.py
@@ -3,7 +3,7 @@ import pathlib
 
 
 def load_tests(_loader, tests, _ignore):
-    path_names = [path.with_suffix('').as_posix().replace('.', '').replace('/', '.').strip('.') for path in pathlib.Path('..').glob(f'*[!venv]*/**/*.py')]
+    path_names = [path.with_suffix('').as_posix().replace('.', '').replace('/', '.').strip('.') for path in pathlib.Path('..').glob(f'*[!.venv]*/**/*.py')]
     for path_name in path_names:
         tests.addTests(doctest.DocTestSuite(path_name))
     return tests
